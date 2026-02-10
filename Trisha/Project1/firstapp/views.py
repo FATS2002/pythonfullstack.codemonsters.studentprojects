8bab74a5c20a9e6977024066ef46a79ecea0c07f
bmtrisha-03 <125240152+bmtrisha-03@users.noreply.github.com>
2026-02-10 16:28:03 +0530

    Add files via upload

---
+++ Trisha/Project1/firstapp/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def display(request):
+    s=s = '''
+    <html>
+    <head>
+        <style>
+            body {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                margin: 0;
+            }
+            .blink {
+                animation: blink 1s linear infinite;
+                color: blue;
+                font-size: 28px;
+                font-weight: bold;
+            }
+            @keyframes blink {
+                50% {
+                    opacity: 0;
+                }
+            }
+        </style>
+    </head>
+    <body>
+        <div class="blink">
+            Hello Students welcome to Sagar Sir Django classes!!!
+        </div>
+    </body>
+    </html>
+    '''
+    return HttpResponse(s)
